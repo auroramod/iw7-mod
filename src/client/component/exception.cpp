25dbb34d59e8fe62ad71eca334cdb103de194357
quaK <38787176+Joelrau@users.noreply.github.com>
2022-05-24 03:34:31 +0300

    Add exception component

---
+++ src/client/component/exception.cpp
@@ -0,0 +1,162 @@
+#include <std_include.hpp>
+#include "loader/component_loader.hpp"
+#include "scheduler.hpp"
+
+#include "game/game.hpp"
+#include "game/dvars.hpp"
+
+#include <utils/hook.hpp>
+#include <utils/io.hpp>
+#include <utils/string.hpp>
+#include <utils/thread.hpp>
+#include <utils/compression.hpp>
+
+#include <exception/minidump.hpp>
+
+#include <version.hpp>
+
+namespace exception
+{
+	namespace
+	{
+		const char* crash_name;
+
+		thread_local struct
+		{
+			DWORD code = 0;
+			PVOID address = nullptr;
+		} exception_data;
+
+		void show_mouse_cursor()
+		{
+			while (ShowCursor(TRUE) < 0);
+		}
+
+		void display_error_dialog()
+		{
+			std::string error_str = utils::string::va("Fatal error (0x%08X) at 0x%p.\n"
+				"A minidump has been written at: %s\n\n",
+				exception_data.code, exception_data.address, crash_name);
+
+			utils::thread::suspend_other_threads();
+			show_mouse_cursor();
+
+			MessageBoxA(nullptr, error_str.data(), "ERROR", MB_ICONERROR);
+			TerminateProcess(GetCurrentProcess(), exception_data.code);
+		}
+
+		void reset_state()
+		{
+			display_error_dialog();
+		}
+
+		size_t get_reset_state_stub()
+		{
+			static auto* stub = utils::hook::assemble([](utils::hook::assembler& a)
+			{
+				a.sub(rsp, 0x10);
+				a.or_(rsp, 0x8);
+				a.jmp(reset_state);
+			});
+
+			return reinterpret_cast<size_t>(stub);
+		}
+
+		std::string get_timestamp()
+		{
+			tm ltime{};
+			char timestamp[MAX_PATH] = { 0 };
+			const auto time = _time64(nullptr);
+
+			_localtime64_s(&ltime, &time);
+			strftime(timestamp, sizeof(timestamp) - 1, "%Y-%m-%d-%H-%M-%S", &ltime);
+
+			return timestamp;
+		}
+
+		std::string generate_crash_info(const LPEXCEPTION_POINTERS exceptioninfo)
+		{
+			std::string info{};
+			const auto line = [&info](const std::string& text)
+			{
+				info.append(text);
+				info.append("\r\n");
+			};
+
+			line("IW7-Mod Crash Dump");
+			line("");
+			line("Version: "s + VERSION);
+			line("Environment: "s + (game::environment::is_dedi() ? "Dedicated" : "Base"));
+			line("Timestamp: "s + get_timestamp());
+			line(utils::string::va("Exception: 0x%08X", exceptioninfo->ExceptionRecord->ExceptionCode));
+			line(utils::string::va("Address: 0x%llX", exceptioninfo->ExceptionRecord->ExceptionAddress));
+
+#pragma warning(push)
+#pragma warning(disable: 4996)
+			OSVERSIONINFOEXA version_info;
+			ZeroMemory(&version_info, sizeof(version_info));
+			version_info.dwOSVersionInfoSize = sizeof(version_info);
+			GetVersionExA(reinterpret_cast<LPOSVERSIONINFOA>(&version_info));
+#pragma warning(pop)
+
+			line(utils::string::va("OS Version: %u.%u", version_info.dwMajorVersion, version_info.dwMinorVersion));
+
+			return info;
+		}
+
+		void write_minidump(const LPEXCEPTION_POINTERS exceptioninfo)
+		{
+			crash_name = utils::string::va("minidumps/iw7mod-crash-%s.zip", get_timestamp().data());
+
+			utils::compression::zip::archive zip_file{};
+			zip_file.add("crash.dmp", create_minidump(exceptioninfo));
+			zip_file.add("info.txt", generate_crash_info(exceptioninfo));
+			zip_file.write(crash_name, "IW7-Mod Crash Dump");
+		}
+
+		bool is_harmless_error(const LPEXCEPTION_POINTERS exceptioninfo)
+		{
+			const auto code = exceptioninfo->ExceptionRecord->ExceptionCode;
+			return code == STATUS_INTEGER_OVERFLOW || code == STATUS_FLOAT_OVERFLOW || code == STATUS_SINGLE_STEP;
+		}
+
+		LONG WINAPI exception_filter(const LPEXCEPTION_POINTERS exceptioninfo)
+		{
+			if (is_harmless_error(exceptioninfo))
+			{
+				return EXCEPTION_CONTINUE_EXECUTION;
+			}
+
+			write_minidump(exceptioninfo);
+
+			exception_data.code = exceptioninfo->ExceptionRecord->ExceptionCode;
+			exception_data.address = exceptioninfo->ExceptionRecord->ExceptionAddress;
+			exceptioninfo->ContextRecord->Rip = get_reset_state_stub();
+
+			return EXCEPTION_CONTINUE_EXECUTION;
+		}
+
+		LPTOP_LEVEL_EXCEPTION_FILTER WINAPI set_unhandled_exception_filter_stub(LPTOP_LEVEL_EXCEPTION_FILTER)
+		{
+			// Don't register anything here...
+			return &exception_filter;
+		}
+	}
+
+	class component final : public component_interface
+	{
+	public:
+		void post_load() override
+		{
+			SetUnhandledExceptionFilter(exception_filter);
+			utils::hook::jump(SetUnhandledExceptionFilter, set_unhandled_exception_filter_stub, true);
+		}
+
+		void post_unpack() override
+		{
+
+		}
+	};
+}
+
+//REGISTER_COMPONENT(exception::component)
