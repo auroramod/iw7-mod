9418fe95fe840a98f23795a628293bf1f1838a2c
Hosseinpourziyaie <hosseinpourziyaie@gmail.com>
2022-10-01 01:19:45 -0700

    Unlock Loot items directly from dw server
    shitty way for unlocking items. it can be used to generate dummie for tracing item flow and pinpoint loot handler functions using debugger

--- src/client/game/demonware/services/bdMarketplace.cpp
+++ src/client/game/demonware/services/bdMarketplace.cpp
@@ -7,8 +7,8 @@ namespace demonware
 {
 	bdMarketplace::bdMarketplace() : service(80, "bdMarketplace")
 	{
-		this->register_task(42, &bdMarketplace::startExchangeTransaction); // COD POINTS purchase ?
-		//this->register_task(43, &bdMarketplace::purchaseOnSteamInitialize); // COD POINTS purchase ?
+		this->register_task(42, &bdMarketplace::startExchangeTransaction);
+		this->register_task(43, &bdMarketplace::purchaseOnSteamInitialize);
 		this->register_task(49, &bdMarketplace::getExpiredInventoryItems);
 		this->register_task(60, &bdMarketplace::steamProcessDurable);
 		this->register_task(85, &bdMarketplace::unk85);
@@ -78,8 +78,32 @@ namespace demonware
 
 	void bdMarketplace::getInventoryPaginated(service_server* server, byte_buffer* /*buffer*/) const
 	{
-		// TODO:
 		auto reply = server->create_reply(this->task_id());
+		
+		std::map<uint32_t, uint32_t> LootID_ranges;
+		LootID_ranges.insert({ 230000, 230011 }); // mp\loot\iw7_cosmetic_heroes_loot_master.csv
+		LootID_ranges.insert({ 110005, 110162 }); // mp\loot\iw7_cosmetic_emotes_loot_master.csv
+		
+		for (auto const& pair : LootID_ranges)
+		{
+			for (size_t loot_id = pair.first; loot_id <= pair.second; loot_id++)
+			{
+				auto loot_item = new bdMarketplaceInventory{};
+				loot_item->m_userID = 0;
+				loot_item->m_accountType = "iw_pc_steam";
+				loot_item->m_itemId = loot_id;
+				loot_item->m_itemQuantity = 1;
+				loot_item->m_itemXp = 0;
+				loot_item->m_itemData = "";
+				loot_item->m_expireDateTime = 0xFFFFFFFF;
+				loot_item->m_expiryDuration = 0x7FFFFFFFFFFFFFFF;
+				loot_item->m_collisionField = 2;
+				loot_item->m_modDateTime = static_cast<uint32_t>(time(nullptr));
+
+				reply->add(loot_item);
+			}
+		}
+
 		reply->send();
 	}
 
