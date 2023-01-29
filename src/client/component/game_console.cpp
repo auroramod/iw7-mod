83deb552fec3f832bde94bc1c61c1e9032c48cba
Seter <86374920+skkuull@users.noreply.github.com>
2023-01-29 04:10:20 +0300

    fix console tab
    Co-Authored-By: fed <58637860+fedddddd@users.noreply.github.com>

--- src/client/component/game_console.cpp
+++ src/client/component/game_console.cpp
@@ -278,17 +278,8 @@ namespace game_console
 			if (matches.size() > 24)
 			{
 				draw_hint_box(1, dvars::con_inputHintBoxColor->current.vector);
-				draw_hint_text(0, utils::string::va("%i matches (too many to show here. Press shift+tilde to open full console)", matches.size()),
+				draw_hint_text(0, utils::string::va("%i matches (too many to show here. press shift+tilde to open full console, press tab to print all matches)", matches.size()),
 					dvars::con_inputDvarMatchColor->current.vector);
-
-				if (GetAsyncKeyState(VK_TAB) & 1)
-				{
-					console::info("]%s\n", con.buffer);
-					for (size_t i = 0; i < matches.size(); i++)
-					{
-						console::info("\t%s\n", matches[i].data());
-					}
-				}
 			}
 			else if (matches.size() == 1)
 			{
@@ -720,6 +711,15 @@ namespace game_console
 					history_index = -1;
 
 					clear();
+				}
+
+				if (key == game::keyNum_t::K_TAB && std::strlen(con.buffer) >= 2 && matches.size() > 24)
+				{
+					console::info("]%s\n", con.buffer);
+					for (const auto& match : matches)
+					{
+						console::info("\t%s\n", match.data());
+					}
 				}
 			}
 		}
