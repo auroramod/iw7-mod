6e9577f76cdd653de16f1ab6ce80fc7dea75d693
m <mjkzyalt@gmail.com>
2026-01-16 15:34:05 -0600

    increase short chat time & height and save

--- src/client/component/chat.cpp
+++ src/client/component/chat.cpp
@@ -1,6 +1,8 @@
 #include <std_include.hpp>
 #include "loader/component_loader.hpp"
 
+#include "dvars.hpp"
+
 #include "game/game.hpp"
 
 #include <utils/hook.hpp>
@@ -25,6 +27,10 @@ namespace chat
 			utils::hook::call(0x14035602C, ui_get_font_handle_stub); // con_drawsay
 			utils::hook::call(0x1409A7843, ui_get_font_handle_stub); // cl_keys_draw_text_override_field
 			utils::hook::call(0x14077D250, ui_get_font_handle_stub); // cg_drawmp_drawchatmessages
+
+			// increase short chat time & height and make them both saved
+			dvars::override::register_int("cg_chatHeight", 5, 0, 8, game::DVAR_FLAG_SAVED);
+			dvars::override::register_int("cg_chatTime", 15000, 0, 60000, game::DVAR_FLAG_SAVED);
 		}
 	};
 }
