c679b4fb2fe8f9bebbd921c39f1dd63c16caffc7
m <mjkzyalt@gmail.com>
2026-09-23 08:54:07 -0500

    fix possible crash with Loot.IsOwned

--- data/cdata/ui_scripts/Patches/__init__.lua
+++ data/cdata/ui_scripts/Patches/__init__.lua
@@ -24,10 +24,12 @@ end
 
 if Loot and Loot.IsOwned then
 	local IsOwned = Loot.IsOwned
-	Loot.IsOwned = function(...)
-		if Engine.GetDvarBool("cg_unlockall_loot") then
-			return true
+	Loot.IsOwned = function(controller, lootId, ...)
+		local count = IsOwned(controller, lootId, ...)
+		-- 30000 (double xp token) crashes, same as stats.cpp item_quantity_stub
+		if lootId ~= 30000 and Engine.GetDvarBool("cg_unlockall_loot") then
+			return math.max(count, 1)
 		end
-		return IsOwned(...)
+		return count
 	end
 end
